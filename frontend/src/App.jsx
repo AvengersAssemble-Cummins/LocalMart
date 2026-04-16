7a2f1641a5139ef3f577fbcac5782e4caac89767
AvengersAssemble-Cummins <purvaabhijitmane@gmail.com>
2026-04-17 02:27:02 +0530

    fix: use explicit .jsx extensions for imports to solve Netlify build errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import Home from './pages/Home';
-import ShopDiscovery from './pages/ShopDiscovery';
-import ShopDetailPage from './pages/ShopDetailPage';
-import ProductListingPage from './pages/ProductListingPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import CartPage from './pages/CartPage';
-import UserProfilePage from './pages/UserProfilePage';
-import CheckoutPage from './pages/CheckoutPage';
-import OrderTrackingPage from './pages/OrderTrackingPage';
-import Navbar from './components/Navbar';
+import Login from './pages/Login.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Home from './pages/Home.jsx';
+import ShopDiscovery from './pages/ShopDiscovery.jsx';
+import ShopDetailPage from './pages/ShopDetailPage.jsx';
+import ProductListingPage from './pages/ProductListingPage.jsx';
+import ProductDetailPage from './pages/ProductDetailPage.jsx';
+import CartPage from './pages/CartPage.jsx';
+import UserProfilePage from './pages/UserProfilePage.jsx';
+import CheckoutPage from './pages/CheckoutPage.jsx';
+import OrderTrackingPage from './pages/OrderTrackingPage.jsx';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
